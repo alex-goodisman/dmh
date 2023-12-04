57eb7f1ace38907aa9ce218d8eb13672713f1278
alex-goodisman <42395598+alex-goodisman@users.noreply.github.com>
2023-12-04 15:10:07 -0500

    I give up, format it differently only on edge

--- client.js
+++ client.js
@@ -428,7 +428,8 @@ async function getState() {
 
 
 	const handsPane = document.getElementById('hands');
-	handsPane.style.display = playerList.length === 0 ? 'inline' : 'initial';
+	// this looks an edge bug but it shuffles the table rows when there's a rowspan in the middle sometimes
+	handsPane.style.display = playerList.length === 0 ? 'inline' : (/Edge/.test(window.navigator.userAgent) ? null : 'initial');
 	// first, remove any extra rows. there should always be at least 1 row in leftElements, so we never remove the top row.
 	Array.from(handsPane.children).filter((_, idx) => idx >= leftElements.length).map(child => child.remove());
 	// then create new rows in case we had too few.
